397a4f7fef829a8096ee277e6a125b8c088d1747
baliram072x <baliramrathod0008@gmail.com>
2026-07-23 21:15:20 +0530

    update

--- src/Constructors.java
+++ src/Constructors.java
@@ -1,17 +1,31 @@
 public class Constructors {
     public static void main(String[]args){
     Students s1 = new Students("BAli");
-        System.out.println(s1.name);
+    Students s2 = new Students();
+    Students s3 = new Students(123);
     }
 }
 
 class Students{
     String name;
     int roll;
-
+//parameterized constructors
+    Students(){
+        System.out.println("constructor is called");
+    }
     Students(String name){
         // this is constuctor
         this.name = name;
-        System.out.println("constructor is called");
+
+    }
+
+    Students(int roll){
+        this.roll  = roll;
     }
 }
+
+// Type of Constructors
+// non parametarized
+// parameterized
+//copy constructor
+
